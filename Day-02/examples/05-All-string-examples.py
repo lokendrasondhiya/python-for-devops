2fc1c9856e996d7b380b60fd118a191577811f63
LOKENDRA SONDHIYA <46724024+lokendrasondhiya@users.noreply.github.com>
2025-08-27 20:11:46 +0000

    concat, len, upper/lower updated

---
+++ Day-02/examples/05-All-string-examples.py
@@ -0,0 +1,16 @@
+str1 ="Lokendra"
+str2 = "Sondhiya"
+
+#concat
+full_name = str1 + " " + str2
+print("Full Name is:", full_name)
+
+#len
+length = len(full_name)
+print("Length of Full Name is :", length)
+
+#lower/upper case
+lowercase = full_name.lower()
+uppercase = full_name.upper()
+print("Name in Lowercase is :",lowercase)
+print("Name in Uppercase is :",uppercase)
